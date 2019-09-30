045c6ec477ea90455cdbd3ad51faa733e992f296
Anders Strömberg <40022844+anst9000@users.noreply.github.com>
2019-09-30 14:17:02 +0200

    Update gulpfile.js
    Change the order of tasks in default function. Now it seems to work as intended.

--- gulpfile.js
+++ gulpfile.js
@@ -1,90 +1,116 @@
 const { src, dest, watch, series, parallel } = require('gulp')
 const concat = require('gulp-concat')
-const uglify = require('gulp-uglify-es').default
+const terser = require('gulp-terser');
 const sass = require('gulp-sass')
 const cleanCSS = require('gulp-clean-css')
 const imagemin = require('gulp-imagemin')
 const del = require('del')
+const babel = require('gulp-babel')
+const sourcemaps = require('gulp-sourcemaps')
 const browserSync = require('browser-sync').create()
 
 const files = {
-    htmlPath: 'src/**/*.html',
-    scssPath: 'src/scss/**/*.scss',
-    jsPath: 'src/js/**/*.js',
-    imgPath: ['src/**/*.{gif,png,jpg,jpeg}'],
-    imgPubPath: 'pub'
+  htmlPath: 'src/**/*.html',
+  scssPath: 'src/scss/**/*.scss',
+  jsPath: 'src/js/**/*.js',
+  resPath: 'src/res/**/*',
+  imgPath: ['src/img/**/*.{gif,png,jpg,jpeg,ico}'],
+  imgPubPath: 'pub/img'
 }
 
 const imgTask = series(imgClean, imgMinify)
 
 // Clean images
 function imgClean() {
-    return del(files.imgPubPath);
+  return del(files.imgPubPath);
 }
 
 // Minifying images by imagemin
 function imgMinify() {
-    // return src('src/img/*')
-    return src(files.imgPath)
-        .pipe(imagemin([
-            imagemin.gifsicle({ interlaced: true }),
-            imagemin.jpegtran({ progressive: true }),
-            imagemin.optipng({ optimizationLevel: 5 }),
-            imagemin.svgo({
-                plugins: [
-                    { removeViewBox: true },
-                    { cleanupIDs: false }
-                ]
-            })
-        ], {
-            verbose: true
-        }))
-        .pipe(dest(files.imgPubPath))  // place images in img-folder
+  // return src('src/img/*')
+  return src(files.imgPath)
+    .pipe(imagemin([
+      imagemin.gifsicle({ interlaced: true }),
+      imagemin.jpegtran({ progressive: true }),
+      imagemin.optipng({ optimizationLevel: 5 }),
+      imagemin.svgo({
+        plugins: [
+          { removeViewBox: true },
+          { cleanupIDs: false }
+        ]
+      })
+    ], {
+      verbose: true
+    }))
+    .pipe(dest(files.imgPubPath))  // place images in img-folder
 }
-
 
 // Task: copy HTML
 function htmlTask() {
-    return src(files.htmlPath)
-        .pipe(dest('pub'))
-        .pipe(browserSync.stream())
+  return src(files.htmlPath)
+    .pipe(dest('pub'))
+    .pipe(browserSync.stream())
 }
 
 // Concatenate and minify js-files
 function jsTask() {
-    return src(files.jsPath)
-        .pipe(concat('main.js'))
-        .pipe(uglify())
-        .pipe(dest('pub/js'))
-        .pipe(browserSync.stream())
+  return src(files.jsPath)
+    .pipe(concat('main.js'))
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(terser())
+    .pipe(dest('pub/js'))
+    .pipe(browserSync.stream())
 }
 
 // Task: Convert scss to css
 function scssTask() {
-    return src(files.scssPath)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(cleanCSS())
-        .pipe(dest('pub/css'))
-        .pipe(browserSync.stream())     // Make sure changes shows in browsers
+  return src(files.scssPath)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(dest('pub/css'))
+    .pipe(browserSync.stream())     // Make sure changes shows in browsers
+}
+
+// Clean and move resources directory
+const resTask = series(resClean, resCopy)
+
+// Clean resources
+function resClean() {
+  return del(files.imgPubPath);
+}
+
+// Task: move resources
+function resCopy() {
+  return src(files.resPath)
+    .pipe(dest('pub/res'))
 }
 
 
 // Task: Watcher
 function watchTask() {
-    browserSync.init({
-        server: {
-            baseDir: 'pub/'
-        }
-    })
+  browserSync.init({
+    server: {
+      baseDir: 'pub/'
+    }
+  })
 
-    watch(
-        [files.htmlPath, files.jsPath, files.scssPath],
-        parallel(htmlTask, jsTask, scssTask)
-    ).on('change', browserSync.reload)
-    watch(files.imgPath, imgTask).on('change', browserSync.reload)
+  watch(
+    [files.htmlPath, files.jsPath, files.scssPath],
+    parallel(htmlTask, jsTask, scssTask)
+  ).on('change', browserSync.reload)
+  watch(files.imgPath, imgTask).on('change', browserSync.reload)
+  watch(files.resPath, resTask).on('change', browserSync.reload)
 }
 
 exports.default = series(
-    parallel(htmlTask, jsTask, scssTask, imgTask),
-    watchTask
+  jsTask,
+  scssTask,
+  htmlTask,
+  resTask,
+  imgTask,
+  watchTask
 )
